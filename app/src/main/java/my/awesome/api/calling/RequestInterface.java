e03b545fc055839ee1e5904109ebbdb6791980de
jeetwide <wide.angle456@gmail.com>
2018-04-18 16:44:32 -0700

    check

--- app/src/main/java/my/awesome/api/calling/RequestInterface.java
+++ app/src/main/java/my/awesome/api/calling/RequestInterface.java
@@ -1,7 +1,10 @@
 package my.awesome.api.calling;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * Created by dd on 09.05.2017.
@@ -14,4 +17,6 @@ public interface RequestInterface {
 
     @GET("api/?key=8716055-bbf7ebb4e2fe4719229979ebe&q=yellow+flowers&image_type=photo&pretty=true")
     Call<JSONResponse> getJSON2();
+
+
 }
